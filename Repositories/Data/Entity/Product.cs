fe715ba7abb5786ea802b662ec38810894fd4d14
Net05_PhanHieuNghia <nghiaphce171837@fpt.edu.vn>
2024-10-02 16:47:04 +0700

    add nullable field

--- Repositories/Data/Entity/Product.cs
+++ Repositories/Data/Entity/Product.cs
@@ -14,7 +14,7 @@ namespace Repositories.Data.Entity
         public float Price { get; set; }
         public string Status { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
         public string CreatedBy { get; set; }
         public string? UpdatedBy { get; set; }
         public string SubCategoryId { get; set; }
