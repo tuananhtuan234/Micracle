32d38f3a54fc0ded699dc91b796ae607546f34c1
Net05_PhanHieuNghia <nghiaphce171837@fpt.edu.vn>
2024-10-06 21:42:58 +0700

    add Description into Product entity and update some function in CardServices

--- Repositories/Data/DTOs/ProductRequestDtos.cs
+++ Repositories/Data/DTOs/ProductRequestDtos.cs
@@ -11,5 +11,6 @@ namespace Repositories.Data.DTOs
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public float Price { get; set; }
+        public string Description { get; set; }
     }
 }
